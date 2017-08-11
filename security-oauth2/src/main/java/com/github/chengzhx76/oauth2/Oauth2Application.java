54fbcd3d38eaec49aa7c31c080294fb494bcb79d
chengzhx76 <chengzhx76@qq.com>
2017-08-11 13:43:19 +0800

    增加Oauth2支持

---
+++ security-oauth2/src/main/java/com/github/chengzhx76/oauth2/Oauth2Application.java
@@ -0,0 +1,18 @@
+package com.github.chengzhx76.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Desc:
+ * Author: chengzhx76@qq.com
+ * Date: 2017/8/11
+ */
+@SpringBootApplication
+public class Oauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Application.class, args);
+    }
+
+}
